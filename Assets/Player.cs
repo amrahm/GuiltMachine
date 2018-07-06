781e18b0ac3b0fab196ffc0fda8ef4dfbcd965e7
Ryan Welch <rlwelch@mit.edu>
2018-07-06 17:20:06 -0400

    Added GameMaster object and script to handle overarching game rules that may not localize well to an object.  Added player script to handle player stats and potentially animations.

---
+++ Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [System.Serializable]
+	public class PlayerStats
+    {
+        public float health = 100f;
+        public float maxHealth = 100f;
+    }
+
+    public PlayerStats playerStats = new PlayerStats();
+
+    void DamagePlayer(float damage)
+    {
+        playerStats.health -= damage;
+        if (playerStats.health <= 0)
+        {
+            GameMaster.KillPlayer(this);
+        }
+    }
+
+    void HealPlayer(float healing)
+    {
+        playerStats.health = Mathf.Max(playerStats.maxHealth, playerStats.health + healing);
+    }
+}
