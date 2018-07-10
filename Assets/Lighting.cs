9c881ec4a117b0b151aa21d98df7dea0bc4fc231
Ali Rahman <amrahman@mit.edu>
2018-07-09 23:57:05 -0500

    Testing new lighting thing

---
+++ Assets/Lighting.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighting : MonoBehaviour {
+    public bool receiveShadow = true;
+
+    private void Start() {
+        GetComponent<Renderer>().receiveShadows = receiveShadow;
+        GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+    }
+}
