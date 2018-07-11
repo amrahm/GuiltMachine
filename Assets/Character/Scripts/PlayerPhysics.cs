7ec1f5246593b02e280e3c02331939cd248cf97d
Ali Rahman <amrahman@mit.edu>
2018-07-10 20:31:43 -0500

    Improved physics. Now uses impulse instead of velocity, and is more generally useable

--- Assets/Character/Scripts/PlayerPhysics.cs
+++ Assets/Character/Scripts/PlayerPhysics.cs
@@ -1,21 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using ExtensionMethods;
-using JetBrains.Annotations;
 using UnityEngine;
 
 public class PlayerPhysics : MonoBehaviour {
-    [SerializeField] private float _crouchAmount;
-
-    [UsedImplicitly] public BodyPartClass hitRotTorso, hitRotHead;
-    [UsedImplicitly] public BodyPartClass hitRotUpperArmR, hitRotUpperArmL, hitRotLowerArmR, hitRotLowerArmL;
-    [UsedImplicitly] public BodyPartClass hitRotThighR, hitRotThighL;
+    [SerializeField] private List<BodyPartClass> _bodyParts;
     public Dictionary<Collider2D, BodyPartClass> collToPart = new Dictionary<Collider2D, BodyPartClass>();
 
     private PlayerMovement _playerMovement; //Reference to PlayerMovement script
-
-    /// <summary> Reference to the player's animator component </summary>
-    private Animator _anim;
 
     /// <summary> Reference to Parts script, which contains all of the player's body parts </summary>
     private Parts _parts;
@@ -24,18 +17,9 @@ public class PlayerPhysics : MonoBehaviour {
     private void Awake() {
         //References
         _playerMovement = GetComponent<PlayerMovement>();
-        _anim = GetComponent<Animator>();
         _parts = GetComponent<Parts>();
 
-
-        hitRotTorso = new BodyPartClass(_parts.torso, false, 1.2f, new List<GameObject> {_parts.torso}, collToPart);
-        hitRotHead = new BodyPartClass(_parts.head, false, 1.2f, new List<GameObject> {_parts.head}, collToPart, hitRotTorso);
-        hitRotUpperArmR = new BodyPartClass(_parts.upperArmR, false, 1f, new List<GameObject> {_parts.upperArmR}, collToPart);
-        hitRotUpperArmL = new BodyPartClass(_parts.upperArmL, false, 1f, new List<GameObject> {_parts.upperArmL}, collToPart);
-        hitRotLowerArmR = new BodyPartClass(_parts.lowerArmR, false, 1f, new List<GameObject> {_parts.lowerArmR, _parts.handR}, collToPart, hitRotUpperArmR);
-        hitRotLowerArmL = new BodyPartClass(_parts.lowerArmL, false, 1f, new List<GameObject> {_parts.lowerArmL, _parts.handL}, collToPart, hitRotUpperArmL);
-        hitRotThighR = new BodyPartClass(_parts.thighR, true, 1.2f, new List<GameObject> {_parts.thighR, _parts.shinR, _parts.footR}, collToPart);
-        hitRotThighL = new BodyPartClass(_parts.thighL, true, 1.2f, new List<GameObject> {_parts.thighL, _parts.shinL, _parts.footL}, collToPart);
+        foreach(var part in _bodyParts) part.Initialize(collToPart, _bodyParts);
     }
 
     private void Update() {
@@ -43,7 +27,10 @@ public class PlayerPhysics : MonoBehaviour {
 //        crouchAmountP -= crouchAmountP * Time.deltaTime * 2;
 //        crouchAmount = crouchAmountSmooth;
 
-        foreach(var part in collToPart.Values) part.StoreForward(part.bodyPart.transform.right, _playerMovement.facingRight);
+        foreach(var part in collToPart.Values) {
+            part.Right = part.bodyPart.transform.right;
+            part.FacingRight = _playerMovement.facingRight;
+        }
     }
 
     private void FixedUpdate() {
@@ -64,14 +51,10 @@ public class PlayerPhysics : MonoBehaviour {
         RotateTo(_parts.hips, _parts.hipsTarget);
 
         foreach(var part in collToPart.Values) {
-            part.HitRot();
-            part.IsTouching();
-            part.postRight = part.bodyPart.transform.right;
+            part.HitRotation();
+            part.HandleTouching();
+            part.PostRight = part.bodyPart.transform.right;
         }
-    }
-
-    private void LateUpdate() {
-//        throw new System.NotImplementedException();
     }
 
     private void RotateTo(GameObject obj, GameObject target) {
@@ -89,46 +72,76 @@ public class PlayerPhysics : MonoBehaviour {
     public class BodyPartClass {
         #region Variables
 
-        [UsedImplicitly] public bool testMode;
-        public readonly GameObject bodyPart;
-        private readonly BodyPartClass _parentPart;
-        private readonly float _partStrength; //1 is standard, 2 is twice as strong
-        private float _upDown;
-        private float _massMult;
-        private Vector3 _positionVector;
-        [SerializeField] private Vector2 _tForceVector, _collisionNormal;
-        [SerializeField] private float _rotAmount, _torqueAmount;
+        [Tooltip("The body part to rotate/control")]
+        public GameObject bodyPart;
 
-        [SerializeField] private bool _shouldHitRot;
-        public bool isTouching;
-        [SerializeField] private readonly bool _isLeg;
-        public Vector3 postRight;
-        private Vector3 _right;
-        private bool _facingRight;
-        private readonly Rigidbody2D _rb;
+        [Tooltip("The parent body part of this body part. Can be null.")] [SerializeField]
+        private GameObject _parentPart;
+
+        [Tooltip("A list of all of the objects that contain colliders for this body part")] [SerializeField]
+        private List<GameObject> _colliderObjects = new List<GameObject>();
+
+        [Tooltip("How resistant this part is to rotation. 1 is standard, 2 is twice as strong")] [SerializeField]
+        private float _partStrength = 1;
+
+        [Tooltip("Is this body part a leg, i.e. should it handle touching the floor differently")] [SerializeField]
+        private bool _isLeg;
+
+        /// <summary> The parent body part class of this body part. Can be null. </summary>
+        private BodyPartClass _parent;
+
+        /// <summary> How far up the length of the body part the collision was </summary>
+        private float _upDown;
+
+        /// <summary> A multiplier from 0 to 1 based on the mass of the colliding object </summary>
+        private float _massMult;
+
+        /// <summary> A vector from the base position of this body part to the point of the collision </summary>
+        private Vector3 _positionVector;
+
+        /// <summary> The normal vector of the collision </summary>
+        private Vector2 _collisionNormal;
+
+        /// <summary> How much this part is rotating beyond normal </summary>
+        private float _rotAmount;
+
+        /// <summary> How much torque is actively being added to rotate this part </summary>
+        private float _torqueAmount;
+
+        /// <summary> Should the hit rotation code run? </summary>
+        private bool _shouldHitRot;
+
+        /// <summary> Is the body part currently touching something? </summary>
+        public bool IsTouching { private get; set; }
+
+        /// <summary> The rightward direction of this bodypart after rotating </summary>
+        public Vector3 PostRight { private get; set; }
+
+        /// <summary> The rightward direction of this bodypart before rotating </summary>
+        public Vector3 Right { private get; set; }
+
+        /// <summary> Is the player facing right? </summary>
+        public bool FacingRight { private get; set; }
+
+        /// <summary> Reference to the rigidbody </summary>
+        private Rigidbody2D _rb;
+
+        /// <summary> Constant multiplier to scale up the rotations </summary>
         private const float Mult = 25f;
+
         private Vector3 _topVector;
 
-        public void StoreForward(Vector3 right, bool facingRight) {
-            _right = right;
-            _facingRight = facingRight;
-        }
-
-        public BodyPartClass(GameObject bodyPart, bool isLeg, float partStrength, IEnumerable<GameObject> colliderObjects,
-            IDictionary<Collider2D, BodyPartClass> collToPart, BodyPartClass parent = null) {
-            this.bodyPart = bodyPart;
+        /// <summary> Adds all of the colliderObjects to a handy dictionary named collToPart.
+        /// Also determines the length of this body part by looking at all of these colliders, thenceby setting _topVector </summary>
+        /// <param name="collToPart">The dictionary to set up</param>
+        /// <param name="bodyParts">A list of all BodyPart classes</param>
+        public void Initialize(IDictionary<Collider2D, BodyPartClass> collToPart, IEnumerable<BodyPartClass> bodyParts) {
+            if(_parentPart != null) _parent = bodyParts.First(part => part.bodyPart == _parentPart);
             _rb = bodyPart.GetComponentInParent<Rigidbody2D>();
-            _partStrength = partStrength;
-            _isLeg = isLeg;
-            _parentPart = parent;
-            FindBounds(colliderObjects, collToPart);
-        }
-
-        private void FindBounds(IEnumerable<GameObject> colliderObjects, IDictionary<Collider2D, BodyPartClass> collToPart) {
             Vector3 objPos = bodyPart.transform.position;
             Vector3 farPoint = objPos;
-            Collider2D farColl = null;
-            foreach(GameObject co in colliderObjects) {
+            Collider2D farColl = bodyPart.GetComponent<Collider2D>();
+            foreach(GameObject co in _colliderObjects) {
                 Collider2D[] colliders = co.GetComponents<Collider2D>();
                 foreach(Collider2D collider in colliders) {
                     if(Vector3.Distance(collider.bounds.center, objPos) >= Vector3.Distance(farPoint, objPos)) {
@@ -138,7 +151,6 @@ public class PlayerPhysics : MonoBehaviour {
                     collToPart.Add(collider, this);
                 }
             }
-            System.Diagnostics.Debug.Assert(farColl != null, nameof(farColl) + " != null");
             farPoint = Vector3.Distance(farPoint + farColl.bounds.extents, objPos) < Vector3.Distance(farPoint - farColl.bounds.extents, objPos)
                            ? bodyPart.transform.InverseTransformPoint(farPoint - farColl.bounds.extents)
                            : bodyPart.transform.InverseTransformPoint(farPoint + farColl.bounds.extents);
@@ -147,14 +159,14 @@ public class PlayerPhysics : MonoBehaviour {
 
         #endregion
 
-        /// <summary> Calculate how much rotation should be added on collision. </summary>
+        /// <summary> Calculate how much rotation should be added on collision </summary>
         /// <param name="point">Point of contact</param>
         /// <param name="direction">Direction of contact</param>
         /// <param name="rVelocity">Relative Velocity of Collision</param>
-        /// <param name="mass">Mass of colliding object</param>
+        /// <param name="mass">Mass of the colliding object</param>
         public void HitCalc(Vector2 point, Vector2 direction, Vector2 rVelocity, float mass) {
             _shouldHitRot = true; //enable HitRot() to apply rotation
-            isTouching = true; //enable IsTouching() for continuous touching
+            IsTouching = true; //enable IsTouching() for continuous touching
             _collisionNormal = direction;
             _positionVector = point - (Vector2) bodyPart.transform.position;
 
@@ -167,31 +179,36 @@ public class PlayerPhysics : MonoBehaviour {
             if(!(Vector3.Dot(direction.normalized, rVelocity.normalized) > 0.01f))
                 return; //Makes sure an object sliding away doesn't cause errant rotations
             Vector2 forceVectorPre = Vector2.Dot(rVelocity, direction) * direction * _upDown;
-            Vector2 forceVector = _isLeg ? Vector2.Dot(forceVectorPre, bodyPart.transform.up) * (Vector2) bodyPart.transform.up : forceVectorPre;
+            Vector2 forceVector = _isLeg ? Vector2.Dot(forceVectorPre, Vector2.right) * Vector2.right : forceVectorPre;
+
             if(_isLeg) {
                 Vector2 crouchVector = forceVectorPre - forceVector;
                 //TODO: Crouch stuff
             }
 
-//            if(bodyPart.name == "ThighL") {
-//                Debug.Log(forceVector);
-//                Debug.DrawRay(bodyPart.transform.position, toTop);
-//            }
-
             AddTorque(rVelocity, mass, forceVector);
         }
 
+        /// <summary> Calculates the torque to add based on the collision force, and transfers the rest of the force to the parent part </summary>
+        /// <param name="rVelocity">Relative Velocity of Collision</param>
+        /// <param name="mass">Mass of the colliding object</param>
+        /// <param name="forceVector">The vector of force from the collision</param>
         private void AddTorque(Vector2 rVelocity, float mass, Vector2 forceVector) {
-            //a vector perpendicular to the normal force and the bodyPart's forward vector to use during rotation. This is torque = r X F
+            //a vector perpendicular to the force and the vector along the body part, which gives the direction to rotate.
             Vector3 cross = Vector3.Cross(_positionVector, forceVector);
 
             _torqueAmount += _massMult * forceVector.magnitude * Mathf.Sign(cross.z);
 
             //force that is parallel to limb or too close to hinge, so can't rotate it, but can be transferred to parent. 	
-            _tForceVector = -0.2f * (rVelocity - forceVector) - _collisionNormal * (forceVector.magnitude - Vector3.Cross(_positionVector, forceVector).magnitude);
-            _parentPart?.HitTransfer(bodyPart.transform.position, rVelocity, _tForceVector, mass);
+            Vector2 transForceVec = -0.2f * (rVelocity - forceVector) - _collisionNormal * (forceVector.magnitude - Vector3.Cross(_positionVector, forceVector).magnitude);
+            _parent?.HitTransfer(bodyPart.transform.position, rVelocity, transForceVec, mass);
         }
 
+        /// <summary> Transfers force from child to parent </summary>
+        /// <param name="point">Point of contact</param>
+        /// <param name="rVelocity">Relative Velocity of Collision</param>
+        /// <param name="transferredForceVector">The vector of force being transferred</param>
+        /// <param name="mass">Mass of the colliding object</param>
         private void HitTransfer(Vector3 point, Vector3 rVelocity, Vector3 transferredForceVector, float mass) {
             _shouldHitRot = true;
             _massMult = Mathf.Exp(-9 / (mass / _partStrength + 2f));
@@ -200,30 +217,32 @@ public class PlayerPhysics : MonoBehaviour {
             AddTorque(unknownVel, mass, transferredForceVector);
         }
 
-        public void HitRot() { //Then apply the rotations calculated
-            if(!_shouldHitRot && !testMode) return;
+        /// <summary> Rotates the body part, dispersing the collision torque over time to return to the resting position </summary>
+        public void HitRotation() {
+            if(!_shouldHitRot) return;
 
-            _rotAmount += _torqueAmount * Time.deltaTime;
+            _rotAmount += _torqueAmount * Time.fixedDeltaTime; //Build up a rotation based on the amount of torque from the collision
             bodyPart.transform.Rotate(Vector3.forward, Mult * _rotAmount, Space.Self);
 
-            if(!testMode) _rotAmount = Extensions.SharpInDamp(_rotAmount, _rotAmount / 2, 0.8f);
-            _torqueAmount -= _torqueAmount * 3 * Time.deltaTime;
+            _torqueAmount -= _torqueAmount * 3 * Time.fixedDeltaTime; //Over time, reduce the torque added from the collision
+            _rotAmount = Extensions.SharpInDamp(_rotAmount, _rotAmount / 2, 0.8f); //and return the body part back to rest
 
-            _shouldHitRot = Mathf.Abs(_rotAmount) * Mult * Mult >= 0.01f;
+            _shouldHitRot = Mathf.Abs(_rotAmount) * Mult * Mult >= 0.01f; //If the rotation is small enough, stop calling this code
         }
 
-        public void IsTouching() { //adjust rotation when rotating into something
-            if(!isTouching || !((_facingRight ? -1 : 1) * Vector3.Dot(_collisionNormal, (postRight - _right).normalized) > 0.1f)) return;
+        /// <summary> Adjusts the rotation of this part when rotating into something that it's touching </summary>
+        public void HandleTouching() {
+            if(!IsTouching || !((FacingRight ? -1 : 1) * Vector3.Dot(_collisionNormal, (PostRight - Right).normalized) > 0.1f)) return;
 
-            float torquePlus = (_facingRight ? 1 : -1) * -2 * Mult * _massMult * _positionVector.magnitude * Vector3.Angle(postRight, _right) / 5 *
-                               Mathf.Sign(Vector3.Cross(_collisionNormal, postRight).z) * _upDown * (_isLeg ? Vector2.Dot(_collisionNormal, Vector2.right) : 1);
+            float torquePlus = (FacingRight ? 1 : -1) * -2 * Mult * _massMult * _positionVector.magnitude * Vector3.Angle(PostRight, Right) / 5 *
+                               Mathf.Sign(Vector3.Cross(_collisionNormal, PostRight).z) * _upDown * (_isLeg ? Vector2.Dot(_collisionNormal, Vector2.right) : 1);
 
-            _torqueAmount += torquePlus * Time.deltaTime;
+            _torqueAmount += torquePlus * Time.fixedDeltaTime;
             _shouldHitRot = true;
-            if(_parentPart != null) {
-                _parentPart._shouldHitRot = true;
+            if(_parent != null) {
+                _parent._shouldHitRot = true;
             }
-            isTouching = false;
+            IsTouching = false;
         }
     }
 
@@ -234,15 +253,17 @@ public class PlayerPhysics : MonoBehaviour {
             foreach(ContactPoint2D c in collInfo.contacts) {
                 if(collToPart.ContainsKey(c.otherCollider)) {
                     BodyPartClass part = collToPart[c.otherCollider];
-                    part.HitCalc(c.point, c.normal, collInfo.relativeVelocity, collInfo.gameObject.GetComponent<Rigidbody2D>().mass);
+                    Vector2 force = float.IsNaN(c.normalImpulse) ? collInfo.relativeVelocity : c.normalImpulse / Time.fixedDeltaTime * c.normal / 1000;
+                    part.HitCalc(c.point, c.normal, force, collInfo.gameObject.GetComponent<Rigidbody2D>().mass);
                 }
             }
         } else {
             foreach(ContactPoint2D c in collInfo.contacts) {
                 if(collToPart.ContainsKey(c.otherCollider)) {
                     BodyPartClass part = collToPart[c.otherCollider];
-                    part.isTouching = true;
-                    part.HitCalc(c.point, c.normal, collInfo.relativeVelocity, 1000);
+                    Vector2 force = float.IsNaN(c.normalImpulse) ? collInfo.relativeVelocity : c.normalImpulse / Time.fixedDeltaTime * c.normal / 1000;
+                    part.IsTouching = true;
+                    part.HitCalc(c.point, c.normal, force, 1000);
                 }
             }
         }
