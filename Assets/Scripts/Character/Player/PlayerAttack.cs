2f100e8253e3974ba304841aa24c5bdff6b72ff7
Ali Rahman <amrahman@mit.edu>
2018-09-28 20:19:32 -0400

    Added some attack animations and started the basic framework for attacking

--- Assets/Scripts/Character/Player/PlayerAttack.cs
+++ Assets/Scripts/Character/Player/PlayerAttack.cs
@@ -19,23 +19,32 @@ public class PlayerAttack : MonoBehaviour {
     /// <summary> Rigidbody component of the gameObject </summary>
     private Rigidbody2D _rb;
 
+    /// <summary> The speed parameter in the animator </summary>
+    private int _jabArmRightAnim;
+
+    /// <summary> The vertical speed parameter in the animator </summary>
+    private int _swingArmRightAnim;
+
     #endregion
 
     private void Awake() {
         _parts = GetComponent<PlayerParts>();
         _anim = GetComponent<Animator>();
         _rb = GetComponent<Rigidbody2D>();
+
+        _jabArmRightAnim = Animator.StringToHash("JabArmRight");
+        _swingArmRightAnim = Animator.StringToHash("SwingArmRight");
     }
 
     public void Attack(float horizontal, float vertical, bool hPressed, bool vPressed, bool hUp, bool vUp) {
-        _anim.ResetTrigger("JabArmRight"); //FIXME? Not sure why I gotta do this, but otherwise the animation plays twice
-        _anim.ResetTrigger("SwingArmRight");
+        _anim.ResetTrigger(_jabArmRightAnim); //FIXME? Not sure why I gotta do this, but otherwise the animation plays twice
+        _anim.ResetTrigger(_swingArmRightAnim);
 
-        if(_attackHoldTime < _tapThreshold && (hUp || vUp)) _anim.SetTrigger("JabArmRight");
+        if(_attackHoldTime < _tapThreshold && (hUp || vUp)) _anim.SetTrigger(_jabArmRightAnim);
 
         if(hPressed || vPressed) {
             if(!_anim.IsInTransition(1))
-                _anim.SetTrigger("SwingArmRight");
+                _anim.SetTrigger(_swingArmRightAnim);
             _attackHoldTime += Time.deltaTime;
         } else {
             _attackHoldTime = 0;
