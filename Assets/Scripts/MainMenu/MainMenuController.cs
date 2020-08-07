650426a864e5de20254dad6b10a9cc5abf20a141
Sarah Yan <syan@wellesley.edu>
2020-08-07 14:20:53 -0700

    owo main menu

---
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void playGame()
+    {
+        SceneManager.LoadScene("TestLevel");
+    }
+
+    // Update is called once per frame
+    public void options()
+    {
+        
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
